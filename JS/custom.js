534471d84d23971a92da73438e43e5ed72a684cc
veganico98 <nicolaslinaresoli@gmail.com>
2025-08-18 12:49:39 -0300

    Ajuste git

--- JS/custom.js
+++ JS/custom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const pokemonName1 = document.querySelector('.pokemonName1');
 const pokemonNumber1 = document.querySelector('.pokemonNumber1');
 const pokemonImage1 = document.querySelector('.pokemonImage1');
@@ -36,44 +35,3 @@ form1.addEventListener('submit', (event) => {
 
     renderPokemonFront(input1.value.toLowerCase());
 })
-
-=======
-const pokemonName1 = document.querySelector('.pokemonName1');
-const pokemonNumber1 = document.querySelector('.pokemonNUmber1');
-const pokemonImage1 = document.querySelector('.pokemonImage1');
-
-const form = document.querySelector('.form');
-const input = document.querySelector('.inputSearch');
-const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('.btn-next');
-
-let searchPokemon1 = 1;
-
-const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}
-`);
-
-    if (APIResponse.status === 200){
-        const data = await APIResponse.json();
-        return data;    
-    }
-}
-
-const renderPokemonFront = async (pokemon) => {
-
-    pokemonName1.innerHTML = 'Loading...';
-    pokemonNumber1.innerHTML = '';
-
-    const data = await fetchPokemon(pokemon);
-
-    pokemonImage1.Style.display = 'block';
-    pokemonImage1.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    pokemonName1.innerHTML = data.name;
-    pokemonNumber1.innerHTML = data.id;
-    searchPokemon1 = data.id
-    input.value = '';
-
-}
-
->>>>>>> f8a5e53a36b022e32bf0cbad6bba53f16181f0fd
-renderPokemonFront(1)
