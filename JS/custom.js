ebf4ed089572c019bbbc95dcbefc70d5caf381ae
veganico98 <nicolaslinaresoli@gmail.com>
2025-08-18 12:39:40 -0300

    Api funcionando

--- JS/custom.js
+++ JS/custom.js
@@ -1,9 +1,9 @@
 const pokemonName1 = document.querySelector('.pokemonName1');
-const pokemonNumber1 = document.querySelector('.pokemonNUmber1');
+const pokemonNumber1 = document.querySelector('.pokemonNumber1');
 const pokemonImage1 = document.querySelector('.pokemonImage1');
 
-const form = document.querySelector('.form');
-const input = document.querySelector('.inputSearch');
+const form1 = document.querySelector('.form1');
+const input1 = document.querySelector('.inputSearch1');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
@@ -20,19 +20,20 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemonFront = async (pokemon) => {
+    const data = await fetchPokemon(pokemon)
 
-    pokemonName1.innerHTML = 'Loading...';
-    pokemonNumber1.innerHTML = '';
-
-    const data = await fetchPokemon(pokemon);
-
-    pokemonImage1.Style.display = 'block';
-    pokemonImage1.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     pokemonName1.innerHTML = data.name;
     pokemonNumber1.innerHTML = data.id;
-    searchPokemon1 = data.id
-    input.value = '';
-
+    pokemonImage1.style.display = 'block';
+    pokemonImage1.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    searchPokemon1 = data.id;
+    input1.value = '';
 }
+
+form1.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemonFront(input1.value.toLowerCase());
+})
 
 renderPokemonFront(1)
